fb2fa74b87d011381a178046ab4c911879dba9d8
Eduardo Gonzalez <wm.eddie@gmail.com>
2023-02-16 01:05:05 +0900

    Add RabbitMQ components.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # xai-rabbitmq
-Xircuits component library for RabbitMQ
+
+A [Xircuits](https://xircuits.io) component library for RabbitMQ
+
+## Installation
+
+You can add this component library to you xai_components folder.
+
+
+## Usage
+
+Use the `RabbitMQConnect` component to connect to the broker.
+
+To subscribe to messages in your xircuits diagram, add a `RabbitMQConsume` node
+and specify a topic. Next connect the components you want to use to process the
+messages to the `on_message` tirangle.
+
+Finally add a `RabbitMQStartConsuming` to begin the processing.  Execution 
+will not continue after the `RabbitMQStartConsuming` component as it loops 
+forever.
+
+To publish a message using xircuits add a `RabbitMQPublish` node to the diagram
+and specify a topic and message to send.  The message will be sent immediately.
+
---
+++ rabbitmq_components.py
@@ -0,0 +1,128 @@
+from xai_components.base import InArg, OutArg, InCompArg, Component, BaseComponent, xai_component
+import pika
+
+
+@xai_component
+class RabbitMQConnect(Component):
+    broker: InArg[str]
+    port: InArg[int]
+    username: InArg[str]
+    password: InArg[str]
+
+    def __init__(self):
+        self.done = False
+        self.broker = InArg.empty()
+        self.port = InArg.empty()
+        self.username = InArg.empty()
+        self.password = InArg.empty()
+
+    def execute(self, ctx) -> None:
+        if self.username.value is not None:
+            credentials = pika.PlainCredentials(self.username.value, self.password.value)
+            parameters = pika.ConnectionParameters(
+                host=self.broker.value,
+                port=self.port.value,
+                virtual_host='/',
+                credentials=credentials)
+            client = pika.BlockingConnection(parameters)
+        else:
+            parameters = pika.ConnectionParameters(
+                host=self.broker.value,
+                port=self.port.value,
+                virtual_host='/')
+            client = pika.BlockingConnection(parameters)
+
+        ctx['rabbitmq_client'] = client
+        ctx['rabbitmq_channel'] = client.channel()
+
+@xai_component
+class RabbitMQPublish(Component):
+    queue: InArg[str]
+    routing_key: InArg[str]
+    exchange: InArg[str]
+    message: InArg[str]
+
+
+    def __init__(self):
+        self.done = False
+        self.queue = InArg.empty()
+        self.routing_key = InArg.empty()
+        self.exchange = InArg.empty()
+        self.message = InArg.empty()
+
+    def execute(self, ctx) -> None:
+        channel = ctx['rabbitmq_channel']
+
+        if ctx.get('rabbitmq_queue') is None:
+            channel.queue_declare(queue=self.queue.value)
+            ctx['rabbitmq_queue'] = self.queue.value
+
+        exchange = '' if self.exchange.value is None else self.exchange.value
+        routing_key = '' if self.routing_key.value is None else self.routing_key.value
+
+        channel.basic_publish(exchange=exchange, routing_key=routing_key, body=self.message.value)
+
+
+@xai_component
+class RabbitMQConsume(Component):
+    on_message: BaseComponent
+    queue: InArg[str]
+    exchange: InArg[str]
+    routing_key: InArg[str]
+    message: OutArg[str]
+
+    def __init__(self):
+        self.done = False
+        self.queue = InArg.empty()
+        self.exchange = InArg.empty()
+        self.routing_key = InArg.empty()
+        self.message = OutArg.empty()
+
+    def execute(self, ctx) -> None:
+        channel = ctx['rabbitmq_channel']
+
+        if ctx.get('rabbitmq_queue') is None:
+            channel.queue_declare(queue=self.queue.value)
+            ctx['rabbitmq_queue'] = self.queue.value
+
+        channel.basic_consume(
+            queue=self.queue.value,
+            on_message_callback=lambda ch, meth, prop, body: self.process_message(ctx, ch, meth, prop, body))
+
+
+    def process_message(self, ctx, channel, method, properties, body):
+        self.message.value = body.decode('utf-8')
+        ctx['rabbitmq_message'] = body
+        ctx['rabbitmq_properties'] = properties
+
+        self.on_message.do(ctx)
+
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+@xai_component
+class RabbitMQStartConsuming(Component):
+    def __init__(self):
+        self.done = False
+
+    def execute(self, ctx) -> None:
+        channel = ctx['rabbitmq_channel']
+
+        try:
+            channel.start_consuming()
+        except Exception as e:
+            print(e)
+
+
+@xai_component
+class RabbitMQDisconnect(Component):
+    def __init__(self):
+        self.done = False
+
+    def execute(self, ctx) -> None:
+        client = ctx['rabbitmq_client']
+
+        try:
+            client.close()
+        except Exception as e:
+            print(e)
